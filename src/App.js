81eabea1eeb4eb1b39e6c904f8874324a85ac877
Mike Cooknick <mcooknick@gmail.com>
2019-11-11 21:00:31 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [phrase, setPhrase] = useState("");
+
+  let upperCase = () => phrase.toUpperCase();
+  let lowerCase = () => phrase.toLowerCase();
+
+  let clapCase = () => {
+    if (phrase.length === 0) {
+      return "";
+    }
+    return phrase.replace(/\s/g, " 👏 ") + " 👏";
+  };
+
+  let spongeCase = () => {
+    let charArray = [];
+
+    if (phrase.length === 0) {
+      return "";
+    }
+
+    for (let i = 0; phrase.length > i; i++) {
+      let singleChar = phrase.charAt(i);
+      if (i % 2 != 0) {
+        charArray.push(singleChar.toUpperCase());
+      } else {
+        charArray.push(singleChar);
+      }
+    }
+    return charArray.join('');
+  };
+
+  const changePhrase = e => {
+    setPhrase(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="App" style={{textAlign:"left"}}>
+      <h1>Case Changer 3000</h1>
+      <p>Input Phase:   
+      <input
+        type="text"
+        onChange={event => changePhrase(event)}
+        placeholder="words"
+      /></p>
+      <hr />
+      <div style={{textAlign:"left"}}>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <b>Clap Case:</b> {clapCase()}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          <b>SpongeBob Case:</b> {spongeCase()}
+        </p>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
